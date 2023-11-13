92a0d7f4d7610ab37568bd8cb2e3113276322485
Wesley Emanuel <wesley.emanuel@prolins.com.br>
2023-11-13 17:27:04 -0300

    test: definicao de variavel de teste global

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -4,7 +4,7 @@ describe("Beach forecast functional tests", () => {
   it("Should return a forecast with just a few times", async () => {
     const { body, status } = await global.testRequest.get("/forecast");
     expect(status).toBe(200);
-    expect(body).toBe([
+    expect(body).toEqual([
       {
         time: "2020-04-26T00:00:00+00:00",
         forecast: [
